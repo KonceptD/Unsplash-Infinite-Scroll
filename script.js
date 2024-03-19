9e0402f9104262aa0d546c8faa623c2679ca1e44
kj <kiranjayd@yahoo.com>
2024-03-19 19:16:02 +0200

    JS: Simplified displayPhotos func, added window event listener for infinite scrolling.

--- script.js
+++ script.js
@@ -10,20 +10,32 @@ const apiKey = 'RVTkMRY3M9EIDu95ISRfVT9ZimeyM_stUa1uF2y9IVY'; // Use 'Access Key
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
  
 
+// Helper function to Set Atrributes on DOM Elements
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+
 // Create elements for links and photos; add to DOM
 function displayPhotos() {
     // run function for each photo in photosArray
     photosArray.forEach((photo) => {
         // create <a> element to link to Unsplash
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
 
         // create image for photo
         const img = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular);
-        img.setAttribute('alt', photo.alt_description);
-        img.setAttribute('title', photo.alt_description);
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
 
         // put <img> inside <a>, then put both inside imageContainer Element
         item.appendChild(img);
@@ -44,6 +56,15 @@ async function getPhotos () {
     }
 }
 
+// Check to see if scrolling at the bottom of the page
+window.addEventListener('scroll', () => {
+    
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000) {
+        getPhotos();
+        console.log('scrolled');
+    }
+});
+
 // On load
 getPhotos(); 
 
@@ -52,5 +73,12 @@ getPhotos();
 NOTES: 
 
 Ctrl + click on variable/element will take you to the root of it, ie where its first instance/creation was made
+
+DRY = Don't Repeat Yourself. Don't use repeated code lines, create functionality to make it simpler. eg: helper function above
+eg:     item.setAttribute('href', photo.links.html);
+        item.setAttribute('target', '_blank');
+        img.setAttribute('src', photo.urls.regular);
+        img.setAttribute('alt', photo.alt_description);
+        img.setAttribute('title', photo.alt_description);
 
 */
